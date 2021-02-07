48a433aab31c93f32e78c4ed5b10acc2f257a1f0
Justin Hu <justin.hu@alumni.ubc.ca>
2021-02-06 22:52:33 -0800

    Simplify board, add healh to ships

--- src/common/Board.hs
+++ src/common/Board.hs
@@ -9,12 +9,6 @@ data Entity
     AstroObj Int Vec2 String Double Double
   | --- Asteroid cluster with some id, some x, y position
     AsteroidCluster Int Vec2
-  | --- Mine with some id, some x, y position and some dx, dy velocity, and some lifetime
-    Mine Int Vec2 Vec2 Int
-  | --- Torpedo with some id, some x, y position and some dx, dy velocity, and some lifetime
-    Torpedo Int Vec2 Vec2 Int
-  | --- Nuke with some id, some x, y position and some dx, dy velocity, and some lifetime
-    Nuke Int Vec2 Vec2 Int
   | --- Ship with:
     ---  - some id
     ---  - some x, y position
@@ -23,22 +17,10 @@ data Entity
     ---  - a name
     ---  - combat strength
     ---  - whether or not it's defensive only
-    ---  - fuel capacity
-    ---  - fuel quantity
-    ---  - cargo capacity
-    ---  - a-list between cargo type and quantity
-    Ship Int Vec2 Vec2 Int String Int Bool Int Int Int [(CargoItem, Int)]
-  | --- Space Base with some id, some x, y position, an owner, and a name
-    SpaceBase Int Vec2 Int String
-  | --- Planetside base with some id, some x, y position, an owner, and a name
-    PlanetBase Int Vec2 Int String
-  deriving (Show)
-
-data CargoItem
-  = --- a mine
-    MineItem
-  | --- a torpedo
-    TorpedoItem
-  | --- a nuke
-    NukeItem
+    ---  - fuel capacity (in delta-v)
+    ---  - fuel quantity (in delta-v)
+    ---  - Weapon health
+    ---  - Drive health
+    ---  - Structure health
+    Ship Int Vec2 Vec2 Int String Int Bool Double Double Int Int Int
   deriving (Show)
