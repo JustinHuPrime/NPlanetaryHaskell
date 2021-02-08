13a83e72e110a20ea4e345fe667d2f80a0b2ce9c
Justin Hu <justin.hu@alumni.ubc.ca>
2021-02-08 15:52:53 -0800

    fix documentation of attack move

--- src/common/Move.hs
+++ src/common/Move.hs
@@ -5,6 +5,6 @@ import Util
 data Move
   = --- thust the ship with given id the given amount
     Thrust Int Vec2
-  | --- launch a mine from the given ship
+  | --- have the first ship attack the second entity (currently must be a ship)
     Attack Int Int
   deriving (Show)
