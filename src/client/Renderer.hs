678fab14f4b3f7dac1e798ed6e069399b12613f6
Johann Cooper <johanngcooper@gmail.com>
2021-02-24 22:06:12 -0800

    refactored renderer and added colors

--- src/client/Renderer.hs
+++ src/client/Renderer.hs
@@ -18,41 +18,45 @@ with N-Planetary. If not, see <https://www.gnu.org/licenses/>.
 
 module Renderer where
 
+import Balance
+import Board
 import Graphics.Rendering.OpenGL.GL
-import Board ( Board, Entity(Ship, AstroObj, AsteroidCluster) )
-import Theme
 import Util
 
-circle :: (Double, Double) -> Double -> Int -> [(Double, Double)]
-circle (x, y) radius vertices = map toPoint angles
+canvasToWorld :: Vec2 -> Vec2
+canvasToWorld (x, y) = (x / mapBorder , y / mapBorder )
+
+renderCircle :: Vec2 -> Double -> Int -> (Double, Double, Double) -> Double -> IO ()
+renderCircle (x, y) radius numVertices (r, g, b) layer  = renderPrimitive TriangleFan (sequence_ [color', vertices])
   where
-    arc       = 2.0 * pi / fromIntegral vertices
+    center    = (x, y)
+    arc       = 2.0 * pi / fromIntegral numVertices
     toPoint a = (x + cos a * radius, y + sin a * radius)
-    angles    = map ((*arc) . fromIntegral) [0..vertices]
-
-renderFan :: [(Double, Double)] -> Double -> IO ()
-renderFan points layer = renderPrimitive TriangleFan $ mapM_ (\(x, y) -> vertex (Vertex3 x y layer)) points
-
-renderCircle :: Vec2 -> Double -> Double -> Int -> IO ()
-renderCircle position radius layer vertices = renderFan (position : circle position radius vertices) layer
-
-renderBoard :: Board -> IO ()
-renderBoard = mapM_ renderEntity
+    angles    = map ((*arc) . fromIntegral) [0..numVertices]
+    points    = center : map toPoint angles
+    vertices  = mapM_ (\(x, y) -> vertex (Vertex3 x y layer)) points
+    color'    = color $ Color3 r g b
 
 -- TODO: add colors from theme
 renderEntity :: Entity -> IO ()
-renderEntity (AstroObj _ (x, y) _ _ radius) = renderCircle position radius' layer 32
+renderEntity (AstroObj _ (x, y) _ _ r) = renderCircle center radius 32 color layer
   where
-    position = (x / fromIntegral windowWidth, y / fromIntegral windowHeight)
-    radius' = radius / fromIntegral windowWidth
+    center = canvasToWorld (x, y)
+    radius = fst (canvasToWorld (r, 0))
+    color = (0.03, 0.56, 0.31)
     layer = -0.9
-renderEntity (AsteroidCluster _ (x, y))          = renderCircle position radius layer 32
+renderEntity (AsteroidCluster _ (x, y)) = renderCircle center radius 32 color layer
   where
-    position = (x / fromIntegral windowWidth, y / fromIntegral windowHeight)
-    radius = 10.0 / fromIntegral windowWidth
+    center = canvasToWorld (x, y)
+    radius = fst (canvasToWorld (1, 0))
+    color = (0.8, 0.8, 0.8)
     layer = -0.9
-renderEntity (Ship _ (x, y) _ _ _ _ _ _ _ _ _ _) = renderCircle position radius layer 32
+renderEntity (Ship _ (x, y) _ owner _ _ _ _ _ _ _ _) = renderCircle center radius 32 color layer
   where
-    position = (x / fromIntegral windowWidth, y / fromIntegral windowHeight)
-    radius = 10.0 / fromIntegral windowWidth
+    center = canvasToWorld (x, y)
+    radius = fst (canvasToWorld (1, 0))
+    color = if owner == 1 then (1.0, 0.25, 0.31) else (0.42, 0.58, 0.94)
     layer = 0.0
+
+renderBoard :: Board -> IO ()
+renderBoard = mapM_ renderEntity
